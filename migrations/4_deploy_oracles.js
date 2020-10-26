bb436ba65a425aa93ee5142801485877edf94edd
Bart Jellema <b@rtje.net>
2020-10-26 16:09:03 +0900

    Remove ChainLinkOracle from deploy

--- migrations/4_deploy_oracles.js
+++ migrations/4_deploy_oracles.js
@@ -1,9 +1,9 @@
 const PeggedOracle = artifacts.require("PeggedOracle");
 const CompoundOracle = artifacts.require("CompoundOracle");
-const ChainLinkOracle = artifacts.require("ChainLinkOracle");
+//const ChainLinkOracle = artifacts.require("ChainLinkOracle");
 
 module.exports = async function (deployer, network, accounts) {
-  await deployer.deploy(PeggedOracle, { overwrite: false });
+  await deployer.deploy(PeggedOracle);
   await deployer.deploy(CompoundOracle);
-  await deployer.deploy(ChainLinkOracle, { overwrite: false });
+  //await deployer.deploy(ChainLinkOracle);
 };
