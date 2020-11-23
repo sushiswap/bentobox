957eac13e59da68fa1d0888482238745d142644f
Johann Barbie <johannbarbie@me.com>
2020-11-23 22:09:35 +0100

    fix most tests

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,14 +1,14 @@
 const BentoBox = artifacts.require("BentoBox");
 const Pair = artifacts.require("LendingPair");
+const WETH9 = artifacts.require("WETH9");
 const SushiSwapDelegateSwapper = artifacts.require("SushiSwapDelegateSwapper");
+const {e18} = require("../test/helpers/utils");
 
-function e18(amount) {
-  return new web3.utils.BN(amount).mul(new web3.utils.BN("1000000000000000000"));
-}
 
 module.exports = async function (deployer, network, accounts) {
   await deployer.deploy(BentoBox, "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2");
   await deployer.deploy(Pair);
+
 
   // Get the contracts
   let bentoBox = await BentoBox.deployed();
