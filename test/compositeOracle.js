f18ef1cf3ce765f0e2b2851db40ba42652d37208
Johann Barbie <johannbarbie@me.com>
2020-11-26 13:08:47 +0100

    fix composite oracle

--- test/compositeOracle.js
+++ test/compositeOracle.js
@@ -9,103 +9,120 @@ const SimpleSLPOracle0 = artifacts.require("SimpleSLPTWAP0Oracle");
 const SimpleSLPOracle1 = artifacts.require("SimpleSLPTWAP1Oracle");
 const CompositeOracle = artifacts.require("CompositeOracle");
 
-const collateralAmount = e18(400); // sushi
-const token1Amount = e18(1);   // eth
-const assetAmount = e18(500); // dai
+const sushiAmount = e18(400); // sushi
+const ethAmount = e18(1);   // eth
+const daiAmount = e18(500); // dai
 const rounding = e9(10000000);
 
 contract('CompositeOracle', (accounts) => {
-  let collateral;
-  let tokenB;
-  let asset;
-  let pairA;
-  let pairB;
-  let oracleA;
-  let oracleB;
+  let sushiToken;
+  let ethToken;
+  let daiToken;
+  let pairSushiEth;
+  let pairDaiEth;
+  let oracleSushiEth;
+  let oracleDaiEth;
   let compositeOracle;
-  let oracleData;
+  let compositeOracleData;
 
   beforeEach(async () => {
 
-    // a is collateral to pairA
-    collateral = await ReturnFalseERC20.new("Token A", "A", e18(10000000), { from: accounts[0] });
-    tokenB = await ReturnFalseERC20.new("Token B", "B", e18(10000000), { from: accounts[0] });
-    asset = await ReturnFalseERC20.new("Token C", "C", e18(10000000), { from: accounts[0] });
+    sushiToken = await ReturnFalseERC20.new("Sushi", "SHI", e18(10000000), { from: accounts[0] });
+    ethToken = await ReturnFalseERC20.new("WETH", "ETH", e18(10000000), { from: accounts[0] });
+    daiToken = await ReturnFalseERC20.new("DAI", "DAI", e18(10000000), { from: accounts[0] });
 
     const factory = await SushiSwapFactory.new(accounts[0], { from: accounts[0] });
 
-    // set up first uni pair
-    let tx = await factory.createPair(collateral.address, tokenB.address);
-    pairA = await UniswapV2Pair.at(tx.logs[0].args.pair);
+    // set up Sushi / Eth uni pair
+    let tx = await factory.createPair(sushiToken.address, ethToken.address);
+    pairSushiEth = await UniswapV2Pair.at(tx.logs[0].args.pair);
 
-    await collateral.transfer(pairA.address, collateralAmount);
-    await tokenB.transfer(pairA.address, token1Amount);
-    await pairA.mint(accounts[0]);
-    if (tokenB.address == (await pairA.token0())) {
-           oracleA = await SimpleSLPOracle0.new();
+    await sushiToken.transfer(pairSushiEth.address, sushiAmount);
+    await ethToken.transfer(pairSushiEth.address, ethAmount);
+    await pairSushiEth.mint(accounts[0]);
+
+    // set up Sushi / Eth oracle 
+    // ETH is the asset
+    // Sushi is the collateral
+    if (ethToken.address == (await pairSushiEth.token0())) {
+           oracleSushiEth = await SimpleSLPOracle0.new();
        } else {
-           oracleA = await SimpleSLPOracle1.new();
+           oracleSushiEth = await SimpleSLPOracle1.new();
     }
-    const oracleDataA = await oracleA.getDataParameter(pairA.address, 0, false);
+    const oracleDataA = await oracleSushiEth.getDataParameter(pairSushiEth.address, 0, false);
 
-    // set up second uni pair
-    tx = await factory.createPair(tokenB.address, asset.address);
-    pairB = await UniswapV2Pair.at(tx.logs[0].args.pair);
+    // set up Dai / Eth uni pair
+    tx = await factory.createPair(ethToken.address, daiToken.address);
+    pairDaiEth = await UniswapV2Pair.at(tx.logs[0].args.pair);
 
-    await tokenB.transfer(pairB.address, token1Amount);
-    await asset.transfer(pairB.address, assetAmount);
-    await pairB.mint(accounts[0]);
+    await ethToken.transfer(pairDaiEth.address, ethAmount);
+    await daiToken.transfer(pairDaiEth.address, daiAmount);
+    await pairDaiEth.mint(accounts[0]);
 
-    if (asset.address == (await pairA.token0())) {
-           oracleB = await SimpleSLPOracle0.new();
+    // set up Dai / Eth oracle
+    // DAI is the asset
+    // ETH is the collateral
+    if (daiToken.address == (await pairDaiEth.token0())) {
+           oracleDaiEth = await SimpleSLPOracle0.new();
        } else {
-           oracleB = await SimpleSLPOracle1.new();
+           oracleDaiEth = await SimpleSLPOracle1.new();
     }
-
-    const oracleDataB = await oracleB.getDataParameter(pairB.address, 0, false);
+    const oracleDataB = await oracleDaiEth.getDataParameter(pairDaiEth.address, 0, false);
 
     // set up composite oracle
+    // Sushi is the collateral
+    // DAI is the asset
     compositeOracle = await CompositeOracle.new();
-    oracleData = await compositeOracle.getDataParameter(oracleA.address, oracleB.address, oracleDataA, oracleDataB);
+    compositeOracleData = await compositeOracle.getDataParameter(oracleSushiEth.address, oracleDaiEth.address, oracleDataA, oracleDataB);
   });
 
   it('update', async () => {
     // update both pairs
-    await compositeOracle.get(oracleData);
+    await compositeOracle.get(compositeOracleData);
     await timeWarp.advanceTime(61);
-    await compositeOracle.get(oracleData);
+    await compositeOracle.get(compositeOracleData);
 
     // check the composite oracle
-    const expectedPrice = encodePrice(collateralAmount, assetAmount);
-    const price = (await compositeOracle.peek(oracleData))[1];
-    console.log(price.toString());
-    assert.equal(price.divRound(rounding).toString(), collateralAmount.mul(new web3.utils.BN("100")).div(assetAmount).toString());
+    const expectedPrice = encodePrice(sushiAmount, daiAmount);
+    const price = (await compositeOracle.peek(compositeOracleData))[1];
+    // oracle returns "the amount of callateral unit to buy 10^18 of asset units"
+    // expectation: 0.8 of Sushi to buy 1 DAI
+    assert.equal(price.divRound(rounding).toString(), '80');
   });
 
   it('should update prices after swap', async () => {
     // update exchange rate
-    await compositeOracle.get(oracleData);
+    await compositeOracle.get(compositeOracleData);
     await timeWarp.advanceTime(61);
-    await compositeOracle.get(oracleData);
+    await compositeOracle.get(compositeOracleData);
 
     // check the composite oracle
-    const price0 = (await compositeOracle.peek(oracleData))[1];
+    const price0 = (await compositeOracle.peek(compositeOracleData))[1];
 
     // check expectations
-    const oldPrice = collateralAmount.mul(new web3.utils.BN("100")).div(assetAmount);
+    const oldPrice = sushiAmount.mul(new web3.utils.BN("100")).div(daiAmount);
     assert.equal(price0.divRound(rounding).toString(), oldPrice.toString());
 
-    // update the exchange rate
-    // double the sushi price
-    await collateral.transfer(pairA.address, e18(400));
-    await pairA.sync();
-    await timeWarp.advanceTime(61);
-    // read exchange rate again
-    await compositeOracle.get(oracleData);
-    let price1 = (await compositeOracle.peek(oracleData))[1];
+    // half the sushi price
+    await timeWarp.advanceTime(30);
+    await sushiToken.transfer(pairSushiEth.address, e18(400));
+    await pairSushiEth.sync();
+    await timeWarp.advanceTime(30);
+    // read exchange rate again half way
+    await compositeOracle.get(compositeOracleData);
+    let price1 = (await compositeOracle.peek(compositeOracleData))[1];
 
     // check expectations
-    const newPrice = oldPrice.add((collateralAmount.mul(new web3.utils.BN("100")).mul(new web3.utils.BN("2"))).div(assetAmount)).divRound(new web3.utils.BN("2"));
-    assert.equal(price1.divRound(rounding).toString(), newPrice.toString(), "prices should be exactly half way between price points");
+    // oracle returns "the amount of callateral unit to buy 10^18 of asset units"
+    // expectation: 1.2 of Sushi to buy 1 DAI
+    assert.equal(price1.divRound(e9(100000000)).toString(), '12', "prices should be exactly half way between price points");
+
+    // read exchange rate with final exchange rate
+    await timeWarp.advanceTime(61);
+    await compositeOracle.get(compositeOracleData);
+    let price2 = (await compositeOracle.peek(compositeOracleData))[1];
+    // oracle returns "the amount of callateral unit to buy 10^18 of asset units"
+    // expectation: 1.6 of Sushi to buy 1 DAI
+    assert.equal(price2.divRound(rounding).toString(), "160");
   });
 });
