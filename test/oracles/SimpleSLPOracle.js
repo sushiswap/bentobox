e82be790f2d575bf86fe8bb37056c05c8beb5b06
BoringCrypto <b@rtje.net>
2021-01-27 21:02:52 +1100

    Test Coverage 100%

--- test/oracles/SimpleSLPOracle.js
+++ test/oracles/SimpleSLPOracle.js
@@ -1,6 +1,6 @@
 const { ethers } = require("hardhat")
 const { expect } = require("chai")
-const { getBigNumber, roundBN, encodePrice, advanceTime, prepare } = require("../utilities")
+const { getBigNumber, roundBN, encodePrice, advanceTime, advanceTimeAndBlock, deploymentsFixture, prepare } = require("../utilities")
 
 describe("SimpleSLPOracle", function () {
   before(async function () {
@@ -17,116 +17,115 @@ describe("SimpleSLPOracle", function () {
   })
 
   beforeEach(async function () {
-    this.weth9 = await this.WETH9Mock.deploy()
-    await this.weth9.deployed()
+    await deploymentsFixture(this, async (cmd) => {
+      this.collateral = await cmd.addToken("Collateral", "C", this.ReturnFalseERC20Mock)
+      this.asset = await cmd.addToken("Asset", "A", this.RevertingERC20Mock)
+      this.sushiSwapPair = await cmd.addPair(this.collateral, this.asset, 5, 10)
+    })
 
-    this.bentoBox = await this.BentoBoxPlus.deploy(this.weth9.address)
-    await this.bentoBox.deployed()
-
-    this.collateral = await this.ReturnFalseERC20Mock.deploy("Token A", "A", getBigNumber(10000000))
-    await this.collateral.deployed()
-
-    this.asset = await this.RevertingERC20Mock.deploy("Token B", "B", getBigNumber(10000000))
-    await this.asset.deployed()
-
-    this.factory = await this.UniswapV2Factory.deploy(this.alice.address)
-    await this.factory.deployed()
-
-    const createPairTx = await this.factory.createPair(this.collateral.address, this.asset.address)
-
-    const sushipair = (await createPairTx.wait()).events[0].args.pair
-
-    this.pair = await this.UniswapV2Pair.attach(sushipair)
-
-    await this.collateral.transfer(this.pair.address, getBigNumber(5))
-    await this.asset.transfer(this.pair.address, getBigNumber(10))
     this.expectedPrice = encodePrice(getBigNumber(5), getBigNumber(10))
 
-    await this.pair.mint(this.alice.address)
-
-    if (this.asset.address == (await this.pair.token0())) {
-      this.oracle = await this.SimpleSLPTWAP0Oracle.deploy()
+    if (this.asset.address == (await this.sushiSwapPair.token0())) {
+      this.oracleF = await this.SimpleSLPTWAP0Oracle.deploy()
+      this.oracleB = await this.SimpleSLPTWAP1Oracle.deploy()
     } else {
-      this.oracle = await this.SimpleSLPTWAP1Oracle.deploy()
+      this.oracleF = await this.SimpleSLPTWAP1Oracle.deploy()
+      this.oracleB = await this.SimpleSLPTWAP0Oracle.deploy()
     }
-    await this.oracle.deployed()
-    this.oracleData = await this.oracle.getDataParameter(this.pair.address)
+    await this.oracleF.deployed()
+    await this.oracleB.deployed()
+    this.oracleData = await this.oracleF.getDataParameter(this.sushiSwapPair.address)
   })
 
-  describe("name", function () {
-    it("should get name", async function () {
-      expect(await this.oracle.name(this.oracleData)).to.be.equal("SushiSwap TWAP")
+  describe("forward oracle", function () {
+    describe("name", function () {
+      it("should get name", async function () {
+        expect(await this.oracleF.name(this.oracleData)).to.be.equal("SushiSwap TWAP")
+      })
+    })
+
+    describe("symbol", function () {
+      it("should get symbol", async function () {
+        expect(await this.oracleF.symbol(this.oracleData)).to.be.equal("S")
+      })
+    })
+
+    describe("peek", function () {
+      it("should return false on first peek", async function () {
+        expect((await this.oracleF.peek(this.oracleData))[1]).to.equal("0")
+      })
+
+      it("should get price even when time since last update is longer than period", async function () {
+        const blockTimestamp = (await this.sushiSwapPair.getReserves())[2]
+
+        await this.oracleF.get(this.oracleData)
+        await this.oracleB.get(this.oracleData)
+        await advanceTime(30, ethers)
+        await this.oracleF.get(this.oracleData)
+        await this.oracleB.get(this.oracleData)
+        await advanceTime(271, ethers)
+        await this.oracleF.get(this.oracleData)
+        await this.oracleB.get(this.oracleData)
+
+        let info = (await this.oracleF.pairs(this.sushiSwapPair.address)).priceAverage.toString()
+        expect(info).to.be.equal(this.expectedPrice[1].toString())
+
+        await advanceTimeAndBlock(301, ethers)
+        
+        expect((await this.oracleF.peek(this.oracleData))[1]).to.be.equal(getBigNumber(1).mul(5).div(10))
+        await this.oracleB.peek(this.oracleData)
+      })
+    })
+
+    describe("get", function () {
+      it("should update and get prices within period", async function () {
+        const blockTimestamp = (await this.sushiSwapPair.getReserves())[2]
+
+        await this.oracleF.get(this.oracleData)
+        await this.oracleB.get(this.oracleData)
+        await advanceTime(30, ethers)
+        await this.oracleF.get(this.oracleData)
+        await this.oracleB.get(this.oracleData)
+        await advanceTime(271, ethers)
+        await this.oracleB.get(this.oracleData)
+        await this.oracleB.get(this.oracleData)
+        await this.oracleF.get(this.oracleData)
+        await this.oracleF.get(this.oracleData)
+
+        let info = (await this.oracleF.pairs(this.sushiSwapPair.address)).priceAverage.toString()
+
+        expect(info).to.be.equal(this.expectedPrice[1].toString())
+        expect((await this.oracleF.peek(this.oracleData))[1]).to.be.equal(getBigNumber(1).mul(5).div(10))
+        await this.oracleB.peek(this.oracleData)
+      })
+
+      it("should update prices after swap", async function () {
+        const blockTimestamp = (await this.sushiSwapPair.getReserves())[2]
+        await this.oracleF.get(this.oracleData)
+        await advanceTime(301, ethers)
+        await this.oracleF.get(this.oracleData)
+
+        const price0 = (await this.oracleF.peek(this.oracleData))[1]
+        await this.collateral.transfer(this.sushiSwapPair.address, getBigNumber(5))
+        await advanceTime(150, ethers)
+        await this.sushiSwapPair.sync()
+        await advanceTime(150, ethers)
+        await this.oracleF.get(this.oracleData)
+        const price1 = (await this.oracleF.peek(this.oracleData))[1]
+
+        expect(price0).to.be.equal(getBigNumber(1).mul(5).div(10))
+        expect(roundBN(price1)).to.be.equal(roundBN(getBigNumber(1).mul(75).div(100)))
+      })
+    })
+
+    it("Assigns name to SushiSwap TWAP", async function () {
+      expect(await this.oracleF.name(this.oracleData)).to.equal("SushiSwap TWAP")
+    })
+
+    it("Assigns symbol to S", async function () {
+      expect(await this.oracleF.symbol(this.oracleData)).to.equal("S")
     })
   })
 
-  describe("symbol", function () {
-    it("should get symbol", async function () {
-      expect(await this.oracle.symbol(this.oracleData)).to.be.equal("S")
-    })
-  })
-  describe("peek", function () {
-    it("should return false on first peek", async function () {
-      expect((await this.oracle.peek(this.oracleData))[1]).to.equal("0")
-    })
-
-    it("should get price even when time since last update is longer than period", async function () {
-      const blockTimestamp = (await this.pair.getReserves())[2]
-
-      await this.oracle.get(this.oracleData)
-      await advanceTime(30, ethers)
-      await this.oracle.get(this.oracleData)
-      await advanceTime(271, ethers)
-      await this.oracle.get(this.oracleData)
-
-      let info = (await this.oracle.pairs(this.pair.address)).priceAverage.toString()
-      await advanceTime(301, ethers)
-
-      expect(info).to.be.equal(this.expectedPrice[1].toString())
-      expect((await this.oracle.peek(this.oracleData))[1]).to.be.equal(getBigNumber(1).mul(5).div(10))
-    })
-  })
-
-  describe("get", function () {
-    it("should update and get prices within period", async function () {
-      const blockTimestamp = (await this.pair.getReserves())[2]
-
-      await this.oracle.get(this.oracleData)
-      await advanceTime(30, ethers)
-      await this.oracle.get(this.oracleData)
-      await advanceTime(271, ethers)
-      await this.oracle.get(this.oracleData)
-      await this.oracle.get(this.oracleData)
-
-      let info = (await this.oracle.pairs(this.pair.address)).priceAverage.toString()
-
-      expect(info).to.be.equal(this.expectedPrice[1].toString())
-      expect((await this.oracle.peek(this.oracleData))[1]).to.be.equal(getBigNumber(1).mul(5).div(10))
-    })
-
-    it("should update prices after swap", async function () {
-      const blockTimestamp = (await this.pair.getReserves())[2]
-      await this.oracle.get(this.oracleData)
-      await advanceTime(301, ethers)
-      await this.oracle.get(this.oracleData)
-
-      const price0 = (await this.oracle.peek(this.oracleData))[1]
-      await this.collateral.transfer(this.pair.address, getBigNumber(5))
-      await advanceTime(150, ethers)
-      await this.pair.sync()
-      await advanceTime(150, ethers)
-      await this.oracle.get(this.oracleData)
-      const price1 = (await this.oracle.peek(this.oracleData))[1]
-
-      expect(price0).to.be.equal(getBigNumber(1).mul(5).div(10))
-      expect(roundBN(price1)).to.be.equal(roundBN(getBigNumber(1).mul(75).div(100)))
-    })
-  })
-
-  it("Assigns name to SushiSwap TWAP", async function () {
-    expect(await this.oracle.name(this.oracleData)).to.equal("SushiSwap TWAP")
-  })
-
-  it("Assigns symbol to S", async function () {
-    expect(await this.oracle.symbol(this.oracleData)).to.equal("S")
-  })
+  describe("backwards oracle", function () {})
 })
--- test/swappers/SushiSwapSwapper.js
+++ test/swappers/SushiSwapSwapper.js
@@ -1,14 +1,52 @@
 const { expect } = require("chai")
-const { prepare } = require("../utilities")
+const { prepare, getBigNumber, deploymentsFixture } = require("../utilities")
 
 // TODO: Can we test this in isolation?
 describe("SushiSwapSwapper", function () {
   before(async function () {
-    await prepare(this, ["SushiSwapSwapper"])
+    await prepare(this, ["SushiSwapSwapper", "ReturnFalseERC20Mock", "RevertingERC20Mock"])
   })
 
   beforeEach(async function () {
-    this.swapper = await this.SushiSwapSwapper.deploy()
+    await deploymentsFixture(this, async (cmd) => {
+      this.a = await cmd.addToken("Token A", "A", this.ReturnFalseERC20Mock)
+      this.b = await cmd.addToken("Token B", "B", this.RevertingERC20Mock)
+      this.sushiSwapPair = await cmd.addPair(this.a, this.b, 50000, 50000)
+    })
+
+    this.swapper = await this.SushiSwapSwapper.deploy(this.bentoBox.address, this.factory.address)
     await this.swapper.deployed()
+  })
+
+  describe("Swap", function () {
+    it("should swap", async function () {
+      await this.a.approve(this.bentoBox.address, getBigNumber(100))
+      await this.bentoBox.deposit(this.a.address, this.alice.address, this.alice.address, getBigNumber(100), 0)
+      await this.bentoBox.transfer(this.a.address, this.alice.address, this.swapper.address, getBigNumber(20))
+      await this.swapper.swap(this.a.address, this.b.address, this.alice.address, 0, getBigNumber(20))
+    })
+
+    it("should swap in opposite direction", async function () {
+      await this.b.approve(this.bentoBox.address, getBigNumber(100))
+      await this.bentoBox.deposit(this.b.address, this.alice.address, this.alice.address, getBigNumber(100), 0)
+      await this.bentoBox.transfer(this.b.address, this.alice.address, this.swapper.address, getBigNumber(20))
+      await this.swapper.swap(this.b.address, this.a.address, this.alice.address, 0, getBigNumber(20))
+    })
+  })
+
+  describe("Swap Exact", function () {
+    it("should swap exact", async function () {
+      await this.a.approve(this.bentoBox.address, getBigNumber(100))
+      await this.bentoBox.deposit(this.a.address, this.alice.address, this.alice.address, getBigNumber(100), 0)
+      await this.bentoBox.transfer(this.a.address, this.alice.address, this.swapper.address, getBigNumber(30))
+      await this.swapper.swapExact(this.a.address, this.b.address, this.alice.address, this.bob.address, getBigNumber(30), getBigNumber(20))
+    })
+
+    it("should swap exact in opposite direction", async function () {
+      await this.b.approve(this.bentoBox.address, getBigNumber(100))
+      await this.bentoBox.deposit(this.b.address, this.alice.address, this.alice.address, getBigNumber(100), 0)
+      await this.bentoBox.transfer(this.b.address, this.alice.address, this.swapper.address, getBigNumber(30))
+      await this.swapper.swapExact(this.b.address, this.a.address, this.alice.address, this.bob.address, getBigNumber(30), getBigNumber(20))
+    })
   })
 })
