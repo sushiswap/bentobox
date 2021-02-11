19ed194e2683124430a1f9a43135346a966d7cb3
pinkiebell <40266861+pinkiebell@users.noreply.github.com>
2021-02-11 09:42:27 +0100

    ISwapper: enable natspec

--- contracts/interfaces/ISwapper.sol
+++ contracts/interfaces/ISwapper.sol
@@ -3,11 +3,11 @@ pragma solidity 0.6.12;
 import "@boringcrypto/boring-solidity/contracts/interfaces/IERC20.sol";
 
 interface ISwapper {
-    // Withdraws 'amountFrom' of token 'from' from the BentoBox account for this swapper
-    // Swaps it for at least 'amountToMin' of token 'to'
-    // Transfers the swapped tokens of 'to' into the BentoBox using a plain ERC20 transfer
-    // Returns the amount of tokens 'to' transferred to BentoBox
-    // (The BentoBox skim function will be used by the caller to get the swapped funds)
+    /// @notice Withdraws 'amountFrom' of token 'from' from the BentoBox account for this swapper.
+    /// Swaps it for at least 'amountToMin' of token 'to'.
+    /// Transfers the swapped tokens of 'to' into the BentoBox using a plain ERC20 transfer.
+    /// Returns the amount of tokens 'to' transferred to BentoBox.
+    /// (The BentoBox skim function will be used by the caller to get the swapped funds).
     function swap(
         IERC20 fromToken,
         IERC20 toToken,
@@ -16,13 +16,14 @@ interface ISwapper {
         uint256 shareFrom
     ) external returns (uint256 extraAmount, uint256 shareTo);
 
-    // Calculates the amount of token 'from' needed to complete the swap (amountFrom), this should be less than or equal to amountFromMax
-    // Withdraws 'amountFrom' of token 'from' from the BentoBox account for this swapper
-    // Swaps it for exactly 'exactAmountTo' of token 'to'
-    // Transfers the swapped tokens of 'to' into the BentoBox using a plain ERC20 transfer
-    // Transfers allocated, but unused 'from' tokens within the BentoBox to 'refundTo' (amountFromMax - amountFrom)
-    // Returns the amount of 'from' tokens withdrawn from BentoBox (amountFrom)
-    // (The BentoBox skim function will be used by the caller to get the swapped funds)
+    /// @notice Calculates the amount of token 'from' needed to complete the swap (amountFrom),
+    /// this should be less than or equal to amountFromMax.
+    /// Withdraws 'amountFrom' of token 'from' from the BentoBox account for this swapper.
+    /// Swaps it for exactly 'exactAmountTo' of token 'to'.
+    /// Transfers the swapped tokens of 'to' into the BentoBox using a plain ERC20 transfer.
+    /// Transfers allocated, but unused 'from' tokens within the BentoBox to 'refundTo' (amountFromMax - amountFrom).
+    /// Returns the amount of 'from' tokens withdrawn from BentoBox (amountFrom).
+    /// (The BentoBox skim function will be used by the caller to get the swapped funds).
     function swapExact(
         IERC20 fromToken,
         IERC20 toToken,
