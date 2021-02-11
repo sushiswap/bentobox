13da1b22c5cddb4863383e751f4c52053626640b
pinkiebell <40266861+pinkiebell@users.noreply.github.com>
2021-02-11 14:25:52 +0100

    more natspec

--- contracts/MasterContractManager.sol
+++ contracts/MasterContractManager.sol
@@ -11,11 +11,11 @@ contract MasterContractManager is BoringOwnable, BoringFactory {
     event LogSetMasterContractApproval(address indexed masterContract, address indexed user, bool approved);
     event LogRegisterProtocol(address indexed protocol);
 
-    // masterContract to user to approval state
+    /// @notice masterContract to user to approval state
     mapping(address => mapping(address => bool)) public masterContractApproved;
-    // masterContract to whitelisted state for approval without signed message
+    /// @notice masterContract to whitelisted state for approval without signed message
     mapping(address => bool) public whitelistedMasterContracts;
-    // user nonces for masterContract approvals
+    /// @notice user nonces for masterContract approvals
     mapping(address => uint256) public nonces;
 
     bytes32 private constant DOMAIN_SEPARATOR_SIGNATURE_HASH = keccak256("EIP712Domain(string name,uint256 chainId,address verifyingContract)");
@@ -35,11 +35,13 @@ contract MasterContractManager is BoringOwnable, BoringFactory {
         DOMAIN_SEPARATOR = keccak256(abi.encode(DOMAIN_SEPARATOR_SIGNATURE_HASH, "BentoBox V2", chainId, address(this)));
     }
 
+    /// @notice Other contracts need to register with this master contract so that users can approve them for the BentoBox.
     function registerProtocol() public {
         masterContractOf[msg.sender] = msg.sender;
         emit LogRegisterProtocol(msg.sender);
     }
 
+    /// @notice Enables or disables a contract for approval without signed message.
     function whitelistMasterContract(address masterContract, bool approved) public onlyOwner {
         // Checks
         require(masterContract != address(0), "MasterCMgr: Cannot approve 0");
@@ -49,6 +51,13 @@ contract MasterContractManager is BoringOwnable, BoringFactory {
         emit LogWhiteListMasterContract(masterContract, approved);
     }
 
+    /// @notice Approves or revokes a `masterContract` access to `user` funds.
+    /// @param user The address of the user that approves or revokes access.
+    /// @param masterContract The address who gains or loses access.
+    /// @param approved If True approves access. If False revokes access.
+    /// @param v Part of the signature. (See EIP-191)
+    /// @param r Part of the signature. (See EIP-191)
+    /// @param s Part of the signature. (See EIP-191)
     // F4 - Check behaviour for all function arguments when wrong or extreme
     // F4: Don't allow masterContract 0 to be approved. Unknown contracts will have a masterContract of 0.
     // F4: User can't be 0 for signed approvals because the recoveredAddress will be 0 if ecrecover fails
