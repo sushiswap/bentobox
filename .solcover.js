d50af922128ecc147c7ba6999bcf7ba1f02a49f7
pinkiebell <40266861+pinkiebell@users.noreply.github.com>
2021-02-15 08:10:38 +0100

    Revert "solcover: ignore cache/"
    This reverts commit 0033ceac16bae65d3a9a42a5e098ebae2f3f5b31.

--- .solcover.js
+++ .solcover.js
@@ -3,7 +3,6 @@ module.exports = {
     testCommand: "npm test",
     compileCommand: "npm run compile",
     skipFiles: [
-        "cache/",
         "flat/",
         "libraries/",
         "mocks/",
