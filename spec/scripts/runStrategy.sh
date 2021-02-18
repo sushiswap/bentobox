6115540a29683c535d44726897558156c1491f43
shellygr <shellygr@users.noreply.github.com>
2021-02-18 11:19:52 +0200

    Certora/bento box spec - more scripts (#75)
    * bentobox spec
    
    * ignore certora intermediate files
    
    * script files
    
    * ci integration
    
    * ci integration
    
    * syntax yml
    
    * trigger GitHub actions
    
    * more scripts
    
    * this was supposed to be fixed
    
    * cleanup
    
    Co-authored-by: Nurit Dor <nurit@certora.com>

--- spec/scripts/runStrategy.sh
+++ spec/scripts/runStrategy.sh
@@ -1,3 +1,3 @@
-certoraRun  spec/harness/SymbolicStrategy.sol:SymbolicStrategy spec/harness/DummyERC20A.sol:DummyERC20A 	spec/harness/Owner.sol:Owner --link SymbolicStrategy:token=DummyERC20A SymbolicStrategy:owner=Owner --solc solc6.12 \
+certoraRun  spec/harness/SymbolicStrategy.sol:SymbolicStrategy spec/harness/DummyERC20A.sol:DummyERC20A spec/harness/Owner.sol:Owner --link SymbolicStrategy:token=DummyERC20A SymbolicStrategy:owner=Owner \
 	--verify SymbolicStrategy:spec/strategy.spec \
 	--cloud --msg "strategy"
