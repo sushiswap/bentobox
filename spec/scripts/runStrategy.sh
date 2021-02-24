69753f37829316991857e61ae73e0dfea7c9d09d
Nurit Dor <nurit@certora.com>
2021-02-24 21:45:33 +0200

    ciMode for better running time

--- spec/scripts/runStrategy.sh
+++ spec/scripts/runStrategy.sh
@@ -1,3 +1,4 @@
 certoraRun  spec/harness/SymbolicStrategy.sol:SymbolicStrategy spec/harness/DummyERC20A.sol:DummyERC20A spec/harness/Owner.sol:Owner --link SymbolicStrategy:token=DummyERC20A SymbolicStrategy:owner=Owner \
 	--verify SymbolicStrategy:spec/strategy.spec \
+	--settings -ciMode \
 	--cloud --msg "strategy"
