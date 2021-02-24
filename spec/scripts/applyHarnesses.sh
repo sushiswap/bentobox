a3b69bf2155a136032233cd591ad716675de9fa8
shellygr <shellygr@users.noreply.github.com>
2021-02-24 12:37:36 +0200

    Certora sync (#80)
    * bentobox spec
    
    * ignore certora intermediate files
    
    * script files
    
    * ci integration
    
    * ci integration
    
    * syntax yml
    
    * trigger GitHub actions
    
    * more scripts
    
    * this was supposed to be fixed
    
    * cleanup
    
    * suggested change, and test in fork
    
    * fix apply harnesses
    
    * should compile now
    
    * fix
    
    * fix declaration for dispatcher to work
    
    * split
    
    Co-authored-by: Nurit Dor <nurit@certora.com>

--- spec/scripts/applyHarnesses.sh
+++ spec/scripts/applyHarnesses.sh
@@ -2,10 +2,12 @@
 sed -i 's/safeT/t/g' contracts/BentoBox.sol
 sed -i 's/safeT/t/g' contracts/LendingPair.sol
 # Virtualize functions
-perl -077 -i -pe 's/\) public allowed\(from\)/\) virtual public allowed\(from\)/g' contracts/BentoBox.sol
-perl -077 -i -pe 's/\) public \{/\) virtual public \{ /g' contracts/BentoBox.sol
-perl -077 -i -pe 's/constructor\(IERC20 wethToken_\) virtual public/constructor\(IERC20 wethToken_\) public/g' contracts/BentoBox.sol
-perl -077 -i -pe 's/\) external payable returns\(/\) external virtual payable returns\(/g' node_modules/@boringcrypto/boring-solidity/contracts/BoringBatchable.sol
-perl -077 -i -pe 's/\) public payable \{/\) public virtual payable \{/g' node_modules/@boringcrypto/boring-solidity/contracts/BoringFactory.sol
-#add transfer function declaration 
-perl -077 -i -pe 's/\}/function transfer\(address to, uint256 amount\) external;\n function transferFrom\(address from, address to, uint256 amount\) external;\n\}/g' node_modules/@boringcrypto/boring-solidity/contracts/interfaces/IERC20.sol
+perl -0777 -i -pe 's/\) public allowed\(from\)/\) virtual public allowed\(from\)/g' contracts/BentoBox.sol
+perl -0777 -i -pe 's/\) public \{/\) virtual public \{ /g' contracts/BentoBox.sol
+# De-virtualize constructor
+perl -0777 -i -pe 's/constructor\(IERC20 wethToken_\) virtual public/constructor\(IERC20 wethToken_\) public/g' contracts/BentoBox.sol
+# Virtualize more functions
+perl -0777 -i -pe 's/\) external payable returns \(/\) external virtual payable returns \(/g' node_modules/@boringcrypto/boring-solidity/contracts/BoringBatchable.sol
+perl -0777 -i -pe 's/\) public payable /\) public virtual payable /g' node_modules/@boringcrypto/boring-solidity/contracts/BoringFactory.sol
+# Add transfer function declaration 
+perl -0777 -i -pe 's/\}/function transfer\(address to, uint256 amount\) external;\n function transferFrom\(address from, address to, uint256 amount\) external;\n\}/g' node_modules/@boringcrypto/boring-solidity/contracts/interfaces/IERC20.sol
