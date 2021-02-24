3cab3cbf8bd7573857884ce822918d5fd1fc54ee
Nurit Dor <nurit@certora.com>
2021-02-24 22:06:34 +0200

    ciMode for better running time

--- spec/scripts/runStrategy.sh
+++ spec/scripts/runStrategy.sh
@@ -1,4 +1,4 @@
 certoraRun  spec/harness/SymbolicStrategy.sol:SymbolicStrategy spec/harness/DummyERC20A.sol:DummyERC20A spec/harness/Owner.sol:Owner --link SymbolicStrategy:token=DummyERC20A SymbolicStrategy:owner=Owner \
 	--verify SymbolicStrategy:spec/strategy.spec \
-	--settings -ciMode \
+	--settings -ciMode=true \
 	--cloud --msg "strategy"
--- spec/scripts/runSushiStrategy.sh
+++ spec/scripts/runSushiStrategy.sh
@@ -4,5 +4,5 @@ spec/harness/Owner.sol \
 --link SushiStrategyHarness:sushi=DummyERC20A SushiStrategyHarness:owner=Owner \
 --verify SushiStrategyHarness:spec/strategy.spec \
 --cloud \
---settings -ciMode \
+--settings -ciMode=true \
 --msg "sushiStrategy"
