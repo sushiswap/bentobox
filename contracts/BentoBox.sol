bb91127570097a91b99ab236275908226e2ec3d5
Matthew Lilley <hello@matthewlilley.com>
2021-12-07 15:55:04 +0000

    Revert "Update license to comply with GPL3 rules"

--- contracts/BentoBox.sol
+++ contracts/BentoBox.sol
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: UNLICENSED
+
 // The BentoBox
 
 //  ▄▄▄▄· ▄▄▄ . ▐ ▄ ▄▄▄▄▄      ▄▄▄▄·       ▐▄• ▄
@@ -8,18 +10,8 @@
 
 // This contract stores funds, handles their transfers, supports flash loans and strategies.
 
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// Copyright (c) 2021 BoringCrypto - All rights reserved
+// Twitter: @Boring_Crypto
 
 pragma solidity 0.6.12;
 pragma experimental ABIEncoderV2;
